56173b9e59fd0e2654ca0b484eb4bad8e7a32068
Jason Noyes <jrnoyes1@gmail.com>
2022-08-04 18:11:23 -0500

    added the edit functionality

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
 const express = require('express')
-const methodOverride = require('method-override')
 const app = express()
+const methodOverride = require('method-override')
+
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
-app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static('public'))
+
+
 app.use('/places', require('./controllers/places'))
 
 
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -8,23 +8,23 @@ function new_form () {
                 <h1>Add a New Place</h1>
                 <form method="POST" action = "/places">
                     <div className="form-group">
-                        <label>Place Name</label>
+                        <label htmlFor='name'>Place Name</label>
                         <input className="form-control" id="name" name ="name" required />
                     </div>
                     <div className="form-group">
-                        <label>Place Picture</label>
+                        <label htmlFor='pic'>Place Picture</label>
                         <input className="form-control" type="url" id="pic" name ="pic" />
                     </div>
                     <div className="form-group">
-                        <label>City</label>
+                        <label htmlFor='city'>City</label>
                         <input className="form-control" id="city" name ="city" />
                     </div>
                     <div className="form-group">
-                        <label>State</label>
+                        <label htmlFor='state'>State</label>
                         <input className="form-control" id="state" name ="state" />
                     </div>
                     <div className="form-group">
-                        <label>Cuisines</label>
+                        <label htmlFor='cuisines'>Cuisines</label>
                         <input className="form-control" id="cuisines" name ="cuisines" required />
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
