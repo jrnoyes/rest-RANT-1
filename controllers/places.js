3014afb712a77ec67fd2541af56f01f6695fef87
Jason Noyes <jrnoyes1@gmail.com>
2022-08-04 13:44:53 -0500

    adding bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -6,13 +6,13 @@ router.get('/', (req,res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: ''
+        pic: '/images/h-tai.jpg'
     }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: ''
+        pic: '/images/cat-cafe.jpg'
     }]
     res.render('places/index', {places})
 })
--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404() {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+            <div>
+                <img src="/images/wrong-way.jpg" alt="wrong way" />
+                <div>
+                Photo by <a href="https://unsplash.com/@randylaybourne?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Randy Laybourne</a> on <a href="https://unsplash.com/s/photos/wrong-path?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+            </div>
             </main>
         </Def>
     )
